de331fbd3b978a822e2f4edaabea900d676941cb
rbx <1186038586@qq.com>
2022-12-10 19:15:00 +0800

    第二次更新

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -9,6 +9,6 @@ package com.atguigu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("第一次修改");
+        System.out.println("第二次更新");
     }
 }
