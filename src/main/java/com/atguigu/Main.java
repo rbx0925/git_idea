b51abae9fd32e273aa2424357a58dcea6e74e76c
rbx <1186038586@qq.com>
2022-12-10 19:31:25 +0800

    测试冲突,master添加

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("第二次更新");
         System.out.println("dev 新增内容");
+        System.out.println("测试冲突,master添加");
     }
 }
